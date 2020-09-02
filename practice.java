0a0a87b17996d70e0c6fa0c11b0e9d892d57bd79
banothe <46104123+banothe@users.noreply.github.com>
2020-09-02 15:36:16 +0530

    Create practice.java

---
+++ practice.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+	    // create a string
+	    String greet = "Hello! World";
+	    String pops="papa";
+	    System.out.println("The string is: " + greet);
+
+	    //checks the string length
+	    System.out.println("The length of the string: " + greet.substring(5));
+	  }
+}
